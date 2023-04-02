748a3cbfca18e762afe597eda6be4fb522d0ca50
DanishOsahan <danishcse091@gmail.com>
2023-04-02 19:46:26 +0530

    made some changes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connectDb = async ()=> {
     try {
-        const conn =  await mongoose.connect('mongodb+srv://Danish:danish3042002@notes.csjlebi.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+        const conn =  await mongoose.connect('mongodb+srv://Danish:danish3042002@cluster0.59kwwks.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+        // mongoose.set('strictQuery', true)
+        mongoose.set('strictQuery', false);
         console.log(`Mongodb connection :${conn}`);
     } catch (error) {
         console.error(`error : ${error.message}`);
